ace4310e96640f7a62560510b3e1d4adbbb6cf26
Joachim MASSON <joachim.masson.17@gmail.com>
2026-01-02 17:06:21 +0100

    fix display on ReviewCard for responsive

--- client/src/components/DisplayAllReviews/DisplayAllReviews.tsx
+++ client/src/components/DisplayAllReviews/DisplayAllReviews.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import ReviewCard from "../ReviewCard/ReviewCard";
 
+import "./DisplayAllReviews.css";
+
 type ReviewI = {
   idreviews: number;
   review_title: string;
@@ -21,9 +23,9 @@ function DisplayAllReviews() {
 
   return (
     <>
-      <ul>
+      <ul className="DisplayAllReviews-list">
         {reviews.map((review) => (
-          <li key={review.idreviews}>
+          <li className="DisplayAllReviews-element-list" key={review.idreviews}>
             <ReviewCard review={review} />
           </li>
         ))}
