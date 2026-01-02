08ff5eb94892a5ba4b7f3e846629edf1fd41da82
Joachim MASSON <joachim.masson.17@gmail.com>
2026-01-02 14:45:24 +0100

    finalized componentFaq with css and responsive.Fix nimor display bug on faq data

--- client/src/components/ComponentFaq/ComponentFaq.tsx
+++ client/src/components/ComponentFaq/ComponentFaq.tsx
@@ -1,58 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ChevronDown, ChevronUp } from "lucide-react";
 
 import "./ComponentFaq.css";
 
-const questions = [
-  {
-    idtable: 1,
-    question: "Comment acheter une montre sur le site l'Ecrin du temps?",
-    answer: "Il suffit de cliquer sur acheter et de se laisser guider ",
-  },
-  {
-    idtable: 2,
-    question: "Que comprend le prix de vente d'une montre",
-    answer:
-      "Il comprend le prix de la montre, le montant de la TVA ainsi qu'une commission pour le site l'Ecrin du temps",
-  },
-];
+// const questions = [
+//   {
+//     idtable: 1,
+//     question: "Comment acheter une montre sur le site l'Ecrin du temps?",
+//     answer: "Il suffit de cliquer sur acheter et de se laisser guider ",
+//   },
+//   {
+//     idtable: 2,
+//     question: "Que comprend le prix de vente d'une montre",
+//     answer:
+//       "Il comprend le prix de la montre, le montant de la TVA ainsi qu'une commission pour le site l'Ecrin du temps",
+//   },
+// ];
+
+interface QuestionsI {
+  idtable1: number;
+  question: string;
+  answer: string;
+}
 
 function ComponentsFaq() {
   const [activeId, setActiveId] = useState<number | null>(null);
-
   const toggleQuestion = (id: number) => {
     setActiveId(activeId === id ? null : id);
   };
 
+  const [questions, setQuestions] = useState<QuestionsI[]>([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/api/faq`)
+      .then((response) => response.json())
+      .then((data: QuestionsI[]) => {
+        setQuestions(data);
+      });
+  }, []);
+
   return (
     <section className="ComponentFaq-section">
       <h1>Vous vous poser une question?</h1>
-      <h2>
+      <h2 className="ComponentFaq-h2">
         Votre question concernant L’Écrin du temps a peut-être déjà été posée.
         Si c’est le cas, vous la trouverez ici.
       </h2>
       <div className="ComponentFaq-question-list">
         {questions.map((item) => (
-          <div
-            key={item.idtable}
-            className={`faq-item ${activeId === item.idtable ? "active" : ""}`}
-          >
+          <div key={item.idtable1} className="faq-item">
             <button
               type="button"
               className="ComponentFaq-question"
-              onClick={() => toggleQuestion(item.idtable)}
+              onClick={() => toggleQuestion(item.idtable1)}
             >
               {item.question}
               <span className="ComponentFaq-icon">
-                {activeId === item.idtable ? <ChevronUp /> : <ChevronDown />}
+                {activeId === item.idtable1 ? <ChevronUp /> : <ChevronDown />}
               </span>
             </button>
-            {activeId === item.idtable && (
-              <div className="ComponentFaq-answer">
-                <p>{item.answer}</p>
-              </div>
-            )}
+
+            <div
+              className={`ComponentFaq-answer ${activeId === item.idtable1 ? "active" : ""}`}
+            >
+              <p>{item.answer}</p>
+            </div>
           </div>
         ))}
       </div>
--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,8 +1,10 @@
 import DisplayAllReviews from "../../components/DisplayAllReviews/DisplayAllReviews";
+import Navbar from "../../components/Navbar/Navbar";
 
 function About() {
   return (
     <div>
+      <Navbar />
       <h1>Page About</h1>
       <DisplayAllReviews />
     </div>
