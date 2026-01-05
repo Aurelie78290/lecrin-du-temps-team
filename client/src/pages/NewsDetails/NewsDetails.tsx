b61971279e15455352f11f1849c528e9481437ec
aurelie_dumotier <aurelie.dumotier@gmail.com>
2026-01-05 16:30:43 +0100

    Adding fields and photo to the details Page

--- client/src/pages/NewsDetails/NewsDetails.tsx
+++ client/src/pages/NewsDetails/NewsDetails.tsx
@@ -1,5 +1,73 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+import "./NewsDetails.css";
+
+interface Article {
+  idarticles: number;
+  article_title: string;
+  photo: string;
+  subtitle: string;
+  release_date: string;
+  content: string;
+  reference_source: string;
+}
+
 function NewsDetails() {
-  return <></>;
+  const [article, setArticle] = useState<Article | null>(null);
+  const { id } = useParams<{ id: string }>();
+  const formatDate = (dateString: string) => {
+    return dateString.split("T")[0];
+  };
+
+  useEffect(() => {
+    if (!id) return;
+    const fetchArticle = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/articles/${id}`,
+        );
+        if (!res.ok) throw new Error("Article non trouvé");
+        const data: Article = await res.json();
+        setArticle(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchArticle();
+  }, [id]);
+
+  if (!article) return <p>Chargement...</p>;
+
+  return (
+    <section className="NewsDetails-section">
+      <h1 className="NewsDetails-title">{article.article_title}</h1>
+      <div className="NewsDetails-references">
+        <p className="NewsDetails-references-content">
+          <strong>Source : </strong>
+          {article.reference_source}
+        </p>
+        <p className="NewsDetail-references-content">
+          <strong>Date : </strong>
+          {formatDate(article.release_date)}
+        </p>
+      </div>
+      <div className="NewsDetails-container">
+        <img
+          src={article.photo}
+          alt={article.article_title}
+          className="NewsDetails-photo"
+        />
+        <div>
+          {/* <p className="NewsDetails-subtitle">
+            <strong>{article.subtitle}</strong>
+          </p> */}
+          <p className="NewsDetails-content">{article.content}</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default NewsDetails;
