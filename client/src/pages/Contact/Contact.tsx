81b0e9e8e5b37a7e5a21b60f10f2f72f743e1bc0
aurelie_dumotier <aurelie.dumotier@gmail.com>
2026-01-07 16:08:37 +0100

    adding ContactForm Component & News-Section correction

--- client/src/pages/Contact/Contact.tsx
+++ client/src/pages/Contact/Contact.tsx
@@ -1,9 +1,11 @@
 import ComponentFaq from "../../components/ComponentFaq/ComponentFaq";
+import ContactForm from "../../components/ContactForm/ContactForm";
 
 function Contact() {
   return (
     <>
       <ComponentFaq />
+      <ContactForm />
     </>
   );
 }
