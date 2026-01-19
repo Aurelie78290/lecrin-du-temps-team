1d5998e0e7f9a251fa29c27928db14052c4e8232
Thomaspupu <thomas.pujol0407@gmail.com>
2026-01-19 16:40:28 +0100

    created review components displaying one of our reviews, the buttons redirect to the article

--- client/src/pages/UserDashboard/UserDashboard.tsx
+++ client/src/pages/UserDashboard/UserDashboard.tsx
@@ -1,10 +1,12 @@
 import UserGraph from "../../components/UserGraph/UserGraph";
+import UserReview from "../../components/UserReview/UserReview";
 import "./UserDashboard.css";
 
 function UserDashboard() {
   return (
     <>
       <UserGraph />
+      <UserReview/>
     </>
   );
 }
