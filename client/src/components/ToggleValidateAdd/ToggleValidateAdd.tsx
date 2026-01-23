4e62e906599d9f5fb3a9b6a3c19999f7372abdf9
Romain Debas <debasromain@gmail.com>
2026-01-23 09:39:59 +0100

    adjusting a component to try a merge with dev

--- client/src/components/ToggleValidateAdd/ToggleValidateAdd.tsx
+++ client/src/components/ToggleValidateAdd/ToggleValidateAdd.tsx
@@ -2,9 +2,8 @@ import emailjs from "emailjs-com";
 import { useState } from "react";
 
 import "./ToggleValidateAdd.css";
-import type { PendinAddsI } from "../ClassifiedAdDetails/ClassifiedAdDetails";
 
-// import type { PendingAddsI } from "../ClassifiedAdDetails/ClassifiedAdDetails";
+import type { PendingAddsI } from "../ClassifiedAdDetails/ClassifiedAdDetails";
 
 interface WatchDetailsProps {
   idwatch?: number | null | undefined;
