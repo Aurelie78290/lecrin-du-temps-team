02a976f40127d99f6874e33795c2186ad4c24d6e
Joachim MASSON <joachim.masson.17@gmail.com>
2026-01-28 15:18:23 +0100

    carrousel ...

--- client/src/components/DashboardRecentOrders/DashboardRecentOrders.tsx
+++ client/src/components/DashboardRecentOrders/DashboardRecentOrders.tsx
@@ -1,8 +1,7 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import useEmblaCarousel from "embla-carousel-react";
 import { useEffect, useState } from "react";
 
 import "./DashboardRecentOrders.css";
-import "@splidejs/react-splide/css";
 
 type RecentOrdersI = {
   idorder: number;
@@ -15,6 +14,7 @@ type RecentOrdersI = {
 
 function DashboardRecentOrders() {
   const [recentOrders, setRecentOrders] = useState<RecentOrdersI[]>([]);
+  const [emblaRef] = useEmblaCarousel({ loop: true, align: "start" });
 
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/api/AdminRecentOrders`)
@@ -25,49 +25,33 @@ function DashboardRecentOrders() {
   }, []);
   console.log(recentOrders);
   return (
-    <article>
-      {/* https://splidejs.com/ */}
-      <p>Activité récente</p>
+    <article className="DashboardRecentOrders-article">
+      <h2>Activité récente</h2>
+      {/* https://www.embla-carousel.com/get-started/react/ */}
       {recentOrders.length === 0 ? (
         <p>Pas d'activité récentes</p>
       ) : (
-        <Splide
-          options={{
-            rewind: true,
-            perPage: 4,
-            perMove: 1,
-            gap: "1rem",
-
-            breakpoints: {
-              1600: {
-                perPage: 3, // Tablettes
-              },
-              992: {
-                perPage: 2, // Petites tablettes / Grands smartphones
-              },
-              768: {
-                perPage: 1, // Smartphones (1 seul élément pour une lecture claire)
-                gap: "0.5rem",
-              },
-            },
-          }}
-          aria-label="transactions récentes"
-        >
-          {recentOrders.map((e) => (
-            <SplideSlide className="dashboardRecentOrdersCard" key={e.idorder}>
-              <p>
-                {new Date(e.purchase_date).toLocaleDateString("fr-FR", {
-                  day: "2-digit",
-                  month: "2-digit",
-                  year: "numeric",
-                })}
-              </p>
-              <p>Prix: {e.price} €</p>
-              <p>Marque: {e.brand}</p>
-              <p>Model: {e.model}</p>
-            </SplideSlide>
-          ))}
-        </Splide>
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {recentOrders.map((e) => (
+              <article
+                className="embla__slide dashboardRecentOrdersCard"
+                key={e.idorder}
+              >
+                <p>
+                  {new Date(e.purchase_date).toLocaleDateString("fr-FR", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  })}
+                </p>
+                <p>Prix: {e.price} €</p>
+                <p>Marque: {e.brand}</p>
+                <p>Model: {e.model}</p>
+              </article>
+            ))}
+          </div>
+        </div>
       )}
     </article>
   );
