70b571a00a8ebd153f37c4497258662523ca5173
Romain Debas <debasromain@gmail.com>
2026-01-29 12:48:31 +0100

    editing the user profil to be able to add a picture, and edit the informations, for the users and for the admin

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"; // Permet d'effectuer des requêtes HTTP //
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http//localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   withCredentials: true, // Ce qui permet d'envoyer et recevoir les cookies //
 });
 
--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,9 +1,13 @@
 // to make the file a module and avoid the TypeScript error
-export type {};
+import type { UserAccount } from "../../modules/User/userRepository";
 
 declare global {
   namespace Express {
     export interface Request {
+      user?: {
+        id: number;
+        role: string;
+      };
       /* ************************************************************************* */
       // Add your custom properties here, for example:
       //
