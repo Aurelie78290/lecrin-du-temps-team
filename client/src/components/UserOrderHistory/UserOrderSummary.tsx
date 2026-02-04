e18809f92ca2b874440ecd1e02c213182cd31a87
Romain Debas <debasromain@gmail.com>
2026-02-04 10:20:46 +0100

    starting the component to add a  previous order into the collection

--- client/src/components/UserOrderHistory/UserOrderSummary.tsx
+++ client/src/components/UserOrderHistory/UserOrderSummary.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import OrderDetailsPopup from "../OrderDetailsPopup/OrderDetailsPopup";
 import "./UserOderSummary.css";
 
 interface Order {
@@ -17,48 +19,64 @@ interface OrderSummaryProps {
 }
 
 const UserOrderSummary = ({ isOpen, onClose, orders }: OrderSummaryProps) => {
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
   if (!isOpen) return null;
 
   return (
-    <div
-      className="orders-overlay"
-      onClick={onClose}
-      onKeyDown={(e) => e.key === "Escape" && onClose}
-    >
+    <>
       <div
-        className="orders-content"
-        onClick={(e) => e.stopPropagation()}
-        onKeyDown={(e) => e.stopPropagation()}
+        className="orders-overlay"
+        onClick={onClose}
+        onKeyDown={(e) => e.key === "Escape" && onClose}
       >
-        <div className="orders-header">
-          <h2> Historique de vos commandes</h2>
-          <button type="button" className="close-btn" onClick={onClose}>
-            Retour
-          </button>
-        </div>
-        <div className="orders-scroll">
-          {orders.map((order) => (
-            <div key={order.id} className="order-row">
-              <img
-                src={`http://localhost:3310${order.watch_photo}`}
-                alt="Montre"
-                className="order-img"
-              />
-              <div className="order-info">
-                <span className="order-brand">{order.brand}</span>
-                <span className="order-name">{order.name}</span>
-                <span className="order-date">
-                  {" "}
-                  Acquise le:
-                  {new Date(order.purchase_date).toLocaleDateString("fr-FR")}
-                </span>
-              </div>
-              <div className="order-price">{order.price.toLocaleString()}€</div>
-            </div>
-          ))}
+        <div
+          className="orders-content"
+          onClick={(e) => e.stopPropagation()}
+          onKeyDown={(e) => e.stopPropagation()}
+        >
+          <div className="orders-header">
+            <h2> Historique de vos commandes</h2>
+            <button type="button" className="close-btn" onClick={onClose}>
+              Retour
+            </button>
+          </div>
+          <div className="orders-scroll">
+            {orders.map((order) => (
+              <button
+                type="button"
+                key={order.id}
+                className="order-row"
+                onClick={() => setSelectedOrder(order)}
+              >
+                <img
+                  src={`http://localhost:3310${order.watch_photo}`}
+                  alt="Montre"
+                  className="order-img"
+                />
+                <div className="order-info">
+                  <span className="order-brand">{order.brand}</span>
+                  <span className="order-name">{order.name}</span>
+                  <span className="order-date">
+                    {" "}
+                    Acquise le:
+                    {new Date(order.purchase_date).toLocaleDateString("fr-FR")}
+                  </span>
+                </div>
+                <div className="order-price">
+                  {order.price.toLocaleString()}€
+                </div>
+              </button>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      {selectedOrder && (
+        <OrderDetailsPopup
+          order={selectedOrder}
+          onClose={() => setSelectedOrder(null)}
+        />
+      )}
+    </>
   );
 };
 
